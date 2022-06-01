75b1d569f3616bf059887e21f5cd71ed13a2261a
master1324 <56083391+master1324@users.noreply.github.com>
2022-06-01 19:49:41 +0300

    Update ErotimatologioApplication.java
    add heroku url

--- src/main/java/com/p16021/ptixiaki/erotimatologio/ErotimatologioApplication.java
+++ src/main/java/com/p16021/ptixiaki/erotimatologio/ErotimatologioApplication.java
@@ -38,7 +38,7 @@ public class ErotimatologioApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200","https://front-end-angular-ptixiaki.herokuapp.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type","Accept",
 				"Jwt-Token","Authorization","Origin, Accept","X-Requested-With","Access-Control-Request-Method","Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Jwt-Token","Authorization",
